d82bb7e3f442b3aa1ec0c2db92eb4135b61bebb6
Abhimanyu35 <22dcs001@nith.ac.in>
2024-02-14 02:28:01 +0530

    functionality pushed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import HomePage from '../components/HomePage';
 import LoginPage from '../components/LoginPage';
 import ConfessionPage from '../components/ConfessionPage';
+
 import { AnimatePresence } from 'framer-motion';
 import Animated from './Animated.jsx'
 import About from '../components/About.jsx'
@@ -29,6 +30,8 @@ const App = () => {
     };
   }, []);
 
+  
+
   return (
       <>
 
@@ -41,27 +44,22 @@ const App = () => {
       </div>
       <div className='sticker'>
         <div style={{ height: '85vh', width: '80vw', marginTop: '5vh', border: '10px solid white', borderRadius: '40px', position: 'relative', backgroundImage: `url('/rose.png')`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-          <AnimatePresence>
-            {scrollPercentage >= 0 && scrollPercentage < 15 && <HomePage />}
-            {scrollPercentage >= 15 && scrollPercentage < 60 && <LoginPage />}
-            {scrollPercentage >= 60 && scrollPercentage < 85 && <ConfessionPage />}
-            {scrollPercentage >= 85 && <About />}
-            </AnimatePresence>
-        </div>
-      </div>
-    </div>
-   <Router>
-    <Routes>
+        <Router>
+       <Routes>
 
-      
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="/LoginPage" element={<LoginPage/>}></Route>
+
+      <Route path="/" element={<HomePage  />}></Route>
+      <Route path="/LoginPage" element={<LoginPage />}></Route>
       <Route path="/confessionPage" element={<confessionPage/>}></Route>
       <Route path="/about" element={<About />}></Route>
      
 
-    </Routes>
-   </Router>
+       </Routes>
+       </Router>
+        </div>
+      </div>
+    </div>
+  
 
 
 
