0366f1b0e2d9d72cab882114790aa8c816b6cc5a
Dev-Avin <avinash.dev.sharmaji@gmail.com>
2024-02-14 08:06:31 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -41,27 +41,22 @@ const App = () => {
       </div>
       <div className='sticker'>
         <div style={{ height: '85vh', width: '80vw', marginTop: '5vh', border: '10px solid white', borderRadius: '40px', position: 'relative', backgroundImage: `url('/rose.png')`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-          <AnimatePresence>
-            {scrollPercentage >= 0 && scrollPercentage < 15 && <HomePage />}
-            {scrollPercentage >= 15 && scrollPercentage < 60 && <LoginPage />}
-            {scrollPercentage >= 60 && scrollPercentage < 85 && <ConfessionPage />}
-            {scrollPercentage >= 85 && <About />}
-            </AnimatePresence>
-        </div>
-      </div>
-    </div>
-   <Router>
+        <Router>
     <Routes>
 
       
       <Route path="/" element={<HomePage />}></Route>
       <Route path="/LoginPage" element={<LoginPage/>}></Route>
-      <Route path="/confessionPage" element={<confessionPage/>}></Route>
+      <Route path="/confessionPage" element={<ConfessionPage/>}></Route>
       <Route path="/about" element={<About />}></Route>
      
 
     </Routes>
    </Router>
+        </div>
+      </div>
+    </div>
+
 
 
 
