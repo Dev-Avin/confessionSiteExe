96070bfd36adde63b4a96ccdb6bbb3bb416863d5
Dev-Avin <148543057+Dev-Avin@users.noreply.github.com>
2024-02-18 09:16:24 +0530

    Update HomePage.jsx

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -26,7 +26,7 @@ const HomePage = () => {
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -50 }}
       transition={{ duration: 0.5 }}
-      className="homeContainer"
+      className="homePageContainer"
     >
       <div className="HomeImage">
         <img src='/heartBoyLowQual-removebg-preview.png' alt="Heart Boy" />
